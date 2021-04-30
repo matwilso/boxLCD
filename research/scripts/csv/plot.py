565fceabfda75a08f8363e3a3c3ebace9199d1a6
Matthew Wilson <mattwilsonmbw@gmail.com>
2021-04-30 16:36:50 -0600

    push change to csv plot

--- research/scripts/csv/plot.py
+++ research/scripts/csv/plot.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import pathlib
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -5,7 +6,10 @@ import numpy as np
 from matplotlib.ticker import StrMethodFormatter
 np.cat = np.concatenate
 plt.style.use('ggplot')
-plt.rcParams.update({'font.size': 18})
+plt.rcParams.update({
+    'font.size': 18,
+    "text.usetex": True,
+    })
 SMALL_SIZE = 10
 #MEDIUM_SIZE = 18
 #BIGGER_SIZE = 22
@@ -22,33 +26,17 @@ path = pathlib.Path('.')
 envs = ['Luxo', 'Urchin']
 modes = ['real','lenv']
 tags = ['eplen', 'epret', 'succreal'][1:]
-mode_map = {'real': 'Real Simulator', 'lenv': 'Learned Simulator'}
+mode_map = {'real': '$\pi$ from Real Sim', 'lenv': '$\pi$ from Learned Sim'}
 tag_map = {'eplen': 'Ep Length', 'epret': 'Ep Return (training)', 'succreal': 'Success Rate (on real)'}
 smooth_map = {'eplen': 0.6, 'epret': 0.6, 'succreal': 0.8}
 
-LuxoReal = dict(
-EpRet=-2.5428032527156175,
-EpLen=13.345999999999997,
-success_rate=0.9990000000000001,
-)
-
-UrchinReal = dict(
-EpRet=-7.8435451961323635,
-EpLen=39.345,
-success_rate=0.9260000000000003,
-)
-
-LuxoLenv = dict(
-EpRet=-3.2117746651813395,
-EpLen=19.345,
-success_rate=0.9880000000000001,
-)
-
-UrchinLenv= dict(
-EpRet=-9.303390638424455,
-EpLen=47.555,
-success_rate=0.8509999999999998,
-)
+mmap = defaultdict(lambda: '')
+mmap.update(**{
+        'Luxorealsuccreal': ' (*0.999)',
+        'Luxolenvsuccreal': ' (*0.988)',
+        'Urchinrealsuccreal' : ' (*0.998)',
+        'Urchinlenvsuccreal' : ' (*0.955)',
+        })
 
 
 def smooth(scalars, weight=0.8):
@@ -68,13 +56,13 @@ for env in envs:
         realfile = path / realname
         realdf = pd.read_csv(realfile)
         plt.plot(realdf.Step, realdf.Value, linewidth=2.0, color='C0', alpha=0.25)
-        plt.plot(realdf.Step, smooth(realdf.Value, weight=smooth_map[tag]), linewidth=2.0, color='C0', label=mode_map['real'])
+        plt.plot(realdf.Step, smooth(realdf.Value, weight=smooth_map[tag]), linewidth=2.0, color='C0', label=mode_map['real']+mmap[f'{env}real{tag}'])
 
         lenvname = f'{env}_lenv_{tag}.csv'
         lenvfile = path / lenvname
         lenvdf = pd.read_csv(lenvfile)
         plt.plot(lenvdf.Step, lenvdf.Value, linewidth=2.0, color='C1', alpha=0.25)
-        plt.plot(lenvdf.Step, smooth(lenvdf.Value, weight=smooth_map[tag]), linewidth=2.0, color='C1', label=mode_map['lenv'])
+        plt.plot(lenvdf.Step, smooth(lenvdf.Value, weight=smooth_map[tag]), linewidth=2.0, color='C1', label=mode_map['lenv']+mmap[f'{env}lenv{tag}'])
 
         plt.title(f'{env} {tag_map[tag]}')
         plt.xlabel('Environment steps')
